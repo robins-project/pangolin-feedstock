39c0c0b07b828abea79ae0585e5bbc962f7448dc
v-turin <vladimir.turin@opencascade.com>
2018-11-08 14:46:49 +0300

    Linux build

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# pangolin-feedstock
+About pangolin
+==============
+
+Home: https://github.com/stevenlovegrove/Pangolin
+
+Package license: MIT
+
+Summary: Pangolin is a lightweight portable rapid development library for managing OpenGL display / interaction and abstracting video input
---
+++ recipe/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
+
+export CFLAGS="$CFLAGS -D__STDC_FORMAT_MACROS=1 -idirafter /usr/include"
+export CXXFLAGS="$CXXFLAGS -D__STDC_FORMAT_MACROS=1 -idirafter /usr/include"
+export LDFLAGS="$LDFLAGS -L/usr/lib/x86_64-linux-gnu -L/usr/lib -fuse-ld=gold"
+
+cmake -G "Ninja" \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX                  \
+      -DCMAKE_PREFIX_PATH=$PREFIX                     \
+      -DCMAKE_BUILD_TYPE:STRING=Release               \
+      -DBUILD_PANGOLIN_GUI:BOOL=ON                    \
+      -DBUILD_PANGOLIN_V4L:BOOL=OFF                   \
+      -DCMAKE_LIBRARY_ARCHITECTURE=x86_64-linux-gnu   \
+      -DOpenGL_GL_PREFERENCE=GLVND                    \
+      ..
+
+ninja install
