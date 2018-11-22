6136cd95593f4d3d580045ec7459930a4209a980
v-turin <vladimir.turin@opencascade.com>
2018-11-22 13:46:13 +0300

    Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,7 @@ export CFLAGS="$CFLAGS -D__STDC_FORMAT_MACROS=1 -idirafter /usr/include"
 export CXXFLAGS="$CXXFLAGS -D__STDC_FORMAT_MACROS=1 -idirafter /usr/include"
 export LDFLAGS="$LDFLAGS -L/usr/lib/x86_64-linux-gnu -L/usr/lib -fuse-ld=gold"
 
+#      -DOpenGL_GL_PREFERENCE=GLVND                    \
 cmake -G "Ninja" \
       -DCMAKE_INSTALL_PREFIX=$PREFIX                  \
       -DCMAKE_PREFIX_PATH=$PREFIX                     \
@@ -16,7 +17,6 @@ cmake -G "Ninja" \
       -DBUILD_PANGOLIN_GUI:BOOL=ON                    \
       -DBUILD_PANGOLIN_V4L:BOOL=OFF                   \
       -DCMAKE_LIBRARY_ARCHITECTURE=x86_64-linux-gnu   \
-      -DOpenGL_GL_PREFERENCE=GLVND                    \
       -DPYTHON_EXECUTABLE=${PYTHON}                   \
       ..
 
